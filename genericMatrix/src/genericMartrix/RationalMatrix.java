24a85a251aafa483e0a109fa6c4bc3500eb2edd4
long-lang <54842741+long-lang@users.noreply.github.com>
2019-09-15 16:45:00 +0800

    Update RationalMatrix.java

--- genericMatrix/src/genericMartrix/RationalMatrix.java
+++ genericMatrix/src/genericMartrix/RationalMatrix.java
@@ -13,13 +13,19 @@ public class RationalMatrix {
 	}
 public void multiplyMatrix(String a[][],String b[][]){
 		
+	String[][] e=new String[2][2];
 		for(int i=0;i<2;i++) {
 			for(int j=0;j<2;j++) {
-				for(int k=0;k<2;k++)
-				d[i][j]+=a[i][k]+b[k][j];
+				for(int k=0;k<2;k++) {
+					e[i][k]=a[i][k]+b[k][j];
+				}
+				for(int m=0;m<2;)
+				{
+					d[i][j]=e[i][m]+e[i][m+1];
+					m=m+2;
+				}
 			}
-		}
-		
+		}	
 	}
 public void print(String[][]a) {
 	for(int i=0;i<2;i++) {
